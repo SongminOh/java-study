cf32cebd459ec6a4842088beed53dfc1d18901f4
SongminOh <ohsong93@gmail.com>
2020-08-01 01:21:31 +0900

    variable 변수

---
+++ 20200702_변수/src/Ex01.java
@@ -0,0 +1,36 @@
+public class Ex01 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		// 변수 선언
+		// 한 클래스, 메소드 내에서 동일한 이름을 가진 
+		// 변수 선언은 1번밖에 못함 (대입용으로 사용만 가능. 선언은 X)
+		int a;
+		a = 3;
+		a = 4;
+		a = 2+7;
+		System.out.println(a);
+		
+		// 상수 선언
+		// 변할 수 없는 수
+		final int b = 7;
+		b = 7;
+		System.out.println(b);
+		
+		// 작명규칙
+		int if =10;
+		int number = 7;
+		int Number = 8;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
